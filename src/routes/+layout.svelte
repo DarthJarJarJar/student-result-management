8cad1d1859f34793dfe4ff910e2018e98eb1b409
Ayaan <88474643+DarthJarJarJar@users.noreply.github.com>
2024-03-17 18:34:33 -0400

    Delete src directory

--- src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid ">
-      <a class="navbar-brand" href="/">Student Result Management</a>
-    </div>
-</nav>
-
-
-<slot></slot>
-
